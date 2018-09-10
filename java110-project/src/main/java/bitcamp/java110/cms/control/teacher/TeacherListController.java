31389e1b36e793f28a7874bb386e9dd43a4ad9ff
seo <arca4tv@gmail.com>
2018-09-10 14:32:34 +0900

    커 맨 드 패 턴 적 용

---
+++ java110-project/src/main/java/bitcamp/java110/cms/control/teacher/TeacherListController.java
@@ -0,0 +1,25 @@
+package bitcamp.java110.cms.control.teacher;
+
+import bitcamp.java110.cms.App;
+import bitcamp.java110.cms.annotation.Component;
+import bitcamp.java110.cms.annotation.RequestMapping;
+import bitcamp.java110.cms.domain.Teacher;
+
+@Component
+public class TeacherListController {
+
+    @RequestMapping("teacher/list")
+    public void printTeachers() {
+        for (int i = 0; i < App.teachers.size(); i++) {
+            Teacher s = App.teachers.get(i);
+            System.out.printf("%d: %s, %s, %s, %s, %d, [%s]\n",
+                    i,
+                    s.getName(), 
+                    s.getEmail(), 
+                    s.getPassword(), 
+                    s.getTel(),
+                    s.getPay(),
+                    s.getSubjects());
+        }
+    }
+}
