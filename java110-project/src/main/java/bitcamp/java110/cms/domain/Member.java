6bbb6251201ace52ca51f859b1ca880aa98bfacc
seo <arca4tv@gmail.com>
2018-09-12 12:24:32 +0900

    .

--- java110-project/src/main/java/bitcamp/java110/cms/domain/Member.java
+++ java110-project/src/main/java/bitcamp/java110/cms/domain/Member.java
@@ -1,9 +1,15 @@
 package bitcamp.java110.cms.domain;
 
-public class Member {
+import java.io.Serializable;
+
+public class Member implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     protected String name;
     protected String email;
-    protected String password;
+    
+    // transient 필드: serialize 대상에서 제외된다. 
+    protected transient String password;
     
     public String getName() {
         return name;
